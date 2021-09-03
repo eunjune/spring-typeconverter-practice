21f551c66ecb0f2ba5020f4b558c7ed41dbf6805
eunjune <iws1605@naver.com>
2021-09-04 00:21:01 +0900

    스프링에 Formatter 적용

--- src/main/java/helo/typeconverter/WebConfig.java
+++ src/main/java/helo/typeconverter/WebConfig.java
@@ -4,6 +4,7 @@ import helo.typeconverter.converter.IntegerToStringConverter;
 import helo.typeconverter.converter.IpPortToStringConverter;
 import helo.typeconverter.converter.StringToIntegerConverter;
 import helo.typeconverter.converter.StringToIpPortConverter;
+import helo.typeconverter.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,9 +14,13 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIpPortConverter());
-        registry.addConverter(new IpPortToStringConverter());
+        // converter와 아래 formatter는 타입변환이 같은데 converter 우선으로 적용되서 주석처리
+//        registry.addConverter(new StringToIpPortConverter());
+//        registry.addConverter(new IpPortToStringConverter());
+
         registry.addConverter(new StringToIntegerConverter());
         registry.addConverter(new IntegerToStringConverter());
+
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
